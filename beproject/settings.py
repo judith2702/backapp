a62efa10bda711fa9605a007f17c0c9108c304ba
Judith soundarya Joseph <87411824+judith2702@users.noreply.github.com>
2026-02-02 14:38:30 +0100

    Update settings.py

--- beproject/settings.py
+++ beproject/settings.py
@@ -154,6 +154,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Add this line for production
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
